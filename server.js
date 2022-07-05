d944a8f3d592e359a48322634e96994d24585009
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-05 06:40:18 +0200

    delete works

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { Book } from './models/Book.js';
+
+mongoose.connect('mongodb://localhost/bookapi');
 
 const app = express();
 const port = 3037;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.send('<h1>Book Site API</h1>');
+});
+
+app.post('/book', async (req, res) => {
+	const book = new Book(req.body);
+	await book.save();
+	res.status(200).json({ message: 'added book', book });
+});
+
+app.delete('/book/:id', async (req, res) => {
+	const id = req.params.id;
+	const book = await Book.find({ _id: id });
+	await Book.deleteOne({ _id: id });
+	console.log(book);
+	res.status(200).json({ message: 'deleted book', book });
 });
 
 app.listen(port, () => {
